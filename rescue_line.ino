1502c2eefa86aa20205816fb1d60f8ea2d8e1633
Gabriel Bermes--Poupeau <gabrielbermesp@gmail.com>
2025-04-11 15:37:34 +0200

    Update rescue_line.ino

--- rescue_line.ino
+++ rescue_line.ino
@@ -1,1 +1,32 @@
-// Code
+#include "MeMCore.h"
+#include "Wire.h"
+
+// Initialisation
+MeDCMotor motor_L(M1);
+MeDCMotor motor_R(M2);
+MeLineFollower lineFinder(PORT_1);
+MeColorSensor colorsensor_L(PORT_2);
+MeColorSensor colorsensor_R(PORT_4);
+
+// Tiles settings
+const int unknown_tile = 0;
+const int forbidden_tile = 1;
+const int robot_position = 2;
+const int sensor_position = 3;
+const int path_tile = 4;
+const int no_path_tile = 5;
+
+int 
+
+void setup()
+{
+  Serial.begin(115200);
+
+  colorsensor_L.SensorInit();
+  colorsensor_R.SensorInit();
+}
+
+void loop()
+{
+
+}
